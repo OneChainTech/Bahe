287209aa4edba94df0fab011984a8123ee37fef7
OneChainTech <44761375+OneChainTech@users.noreply.github.com>
2023-08-24 20:17:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from PIL import Image
+from gradio_client import Client
 
 # 标题
 # st.title('巴小赫')
-st.markdown("<div style='text-align: left; font-size: 20px;'>巴小赫</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: left; font-size: 20px; margin-bottom: 15px;'>巴小赫</div>", unsafe_allow_html=True)
 
 # 图片封面
 image = Image.open('cover.png') # 请替换为实际图片路径
@@ -26,7 +27,7 @@ music_description = st.text_area('', height=100, max_chars=300)
 st.write('点击生成 30s 音乐')
 # generate_button = st.button('生成音乐')
 # 使用列来居中按钮
-col1, col2, col3 = st.columns([1,1,1])
+col1, col2, col3 = st.columns([1,3,1])
 
 with col2:
     button = st.button('生成音乐')
@@ -35,6 +36,13 @@ with col2:
 if button:
     st.write('音乐生成中...')
     # 在此处添加音乐生成的代码
+    client = Client("https://facebook-musicgen--xstnr.hf.space/")
+    result = client.predict(
+    				"Howdy!",	# str  in 'Describe your music' Textbox component
+    				"https://github.com/gradio-app/gradio/raw/main/test/test_files/audio_sample.wav",	# str (filepath or URL to file) in 'File' Audio component
+    				fn_index=0
+    )
+    print(result)
 
 # 我的创作标题
 # st.title('我的创作')
