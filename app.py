fc2a4621c9b53b79687f603ed00037c1e29d7210
OneChainTech <44761375+OneChainTech@users.noreply.github.com>
2023-08-24 20:08:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 # 标题
 # st.title('巴小赫')
-st.markdown("<h2 style='text-align: center;'>巴小赫</h2>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: left;'>巴小赫</h3>", unsafe_allow_html=True)
 
 # 图片封面
 image = Image.open('cover.png') # 请替换为实际图片路径
@@ -24,11 +24,21 @@ music_description = st.text_area('', height=100, max_chars=300)
 
 # 生成音乐按钮
 st.write('点击生成 30s 音乐')
-generate_button = st.button('生成音乐')
+# generate_button = st.button('生成音乐')
+# 使用列来居中按钮
+col1, col2, col3 = st.beta_columns([1,1,1])
+
+with col2:
+    button = st.button('生成音乐')
+
+# 按钮点击事件
+if button:
+    st.write('音乐生成中...')
+    # 在此处添加音乐生成的代码
 
 # 我的创作标题
 # st.title('我的创作')
-st.markdown("<h2 style='text-align: center;'>我的创作</h2>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: left;'>我的创作</h3>", unsafe_allow_html=True)
 
 
 # 用户生成音乐媒体列表
